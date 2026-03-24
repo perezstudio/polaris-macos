bb3b12f6e8283d781ea07dedfab30722ca88a4a2
Kevin Perez <kp952547@gmail.com>
2026-03-23 22:42:22 -0500

    Initial Commit

---
+++ PolarisTests/PolarisTests.swift
@@ -0,0 +1,17 @@
+//
+//  PolarisTests.swift
+//  PolarisTests
+//
+//  Created by Keviruchis on 3/23/26.
+//
+
+import Testing
+@testable import Polaris
+
+struct PolarisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
